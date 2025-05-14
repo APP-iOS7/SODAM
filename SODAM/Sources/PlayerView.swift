f7f8b94741f4faa14955620c8ed6b9bc0171b60f
hajinCHOI <cresent0203@gmail.com>
2025-05-14 17:32:12 +0900

    FEAT: Player 기본 UI세팅

--- SODAM/Sources/PlayerView.swift
+++ SODAM/Sources/PlayerView.swift
@@ -8,8 +8,91 @@
 import SwiftUI
 
 struct PlayerView: View {
+    let spot: Spot = Spot(title: "창덕궁", address: "서울특별시 종로구", position: 0.0, audioTitle: "고대 중세 한국사 속으로")
+    @State var isAudioOn: Bool = false
+    @State var isLongVer: Bool = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if isLongVer {
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(Color.primaryColor.opacity(0.8))
+                    .frame(height: 60)
+                    .padding(5)
+                    .overlay(
+                        HStack {
+                            AsyncImage(url: URL(string: spot.imageUrl)) { image in
+                                image.resizable()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .cornerRadius(10)
+                            .frame(width: 45,height: 45)
+                            .onTapGesture {
+                                isLongVer.toggle()
+                            }
+                            .padding(.leading, 13)
+                            Text(spot.title)
+                                .font(.headline)
+                                .foregroundStyle(Color.white)
+                                .padding(5)
+                            Spacer()
+                            
+                            if isAudioOn {
+                                Button {
+                                    //TODO: 오디오 재생 함수 필요
+                                    isAudioOn.toggle()
+                                } label: {
+                                    Image(systemName: "pause.fill")
+                                        .font(.title)
+                                        .foregroundStyle(Color.white)
+                                        .padding(.trailing, 10)
+                                        
+                                }
+                                Button {
+                                    //TODO: 플레이어 닫기 함수
+                                } label: {
+                                    Image(systemName: "xmark")
+                                        .font(.title3)
+                                        .foregroundStyle(Color.white)
+                                        .padding(.trailing, 30)
+                                }
+                            } else {
+                                Button {
+                                    //TODO: 오디오 일시정지 함수 필요
+                                    isAudioOn.toggle()
+                                } label: {
+                                    Image(systemName: "play.fill")
+                                        .font(.title)
+                                        .foregroundStyle(Color.white)
+                                        .padding(.trailing, 30)
+                                }
+                            }
+                        }
+                    )
+            } else {
+                HStack {
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color.primaryColor.opacity(0.8))
+                        .frame(width: 60, height: 60)
+                        .padding(5)
+                        .overlay(
+                            HStack {
+                                AsyncImage(url: URL(string: spot.imageUrl)) { image in
+                                    image.resizable()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .cornerRadius(10)
+                                .frame(width: 45,height: 45)
+                                .onTapGesture {
+                                    isLongVer.toggle()
+                                }
+                            }
+                        )
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
