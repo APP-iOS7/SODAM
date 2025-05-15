c174f1a7c115531982cdbfd3e3b9ff99aec437a2
hiereit-dev <https://github.com/hiereit-dev>
2025-05-15 09:24:53 +0900

    REFACTOR: @main App 구조체에서 ModelContainer를 초기화하고 주입

--- SODAM/Sources/SODAMApp.swift
+++ SODAM/Sources/SODAMApp.swift
@@ -1,10 +1,18 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct SODAMApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([VisitedPlaceItem.self])
+        let modelConfiguration = ModelConfiguration(schema: schema)
+        return try! ModelContainer(for: schema, configurations: [modelConfiguration])
+    }()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(sharedModelContainer)
     }
 }
