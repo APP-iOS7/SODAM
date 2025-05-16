dff4a563ead65555b0515093f802dbb734024d65
김용해 <yoho1019@gmail.com>
2025-05-16 10:36:14 +0900

    STYLE: Assets 지도 및 Menu 아이콘 추가

---
+++ SODAM/Sources/RegionalListView.swift
@@ -0,0 +1,18 @@
+//
+//  RegionalListView.swift
+//  SODAM
+//
+//  Created by 김용해 on 5/15/25.
+//
+
+import SwiftUI
+
+struct RegionalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegionalListView()
+}
