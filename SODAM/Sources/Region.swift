51744e5391a1a536ad5e178cd90a0647d6f675ac
김용해 <yoho1019@gmail.com>
2025-05-16 10:37:23 +0900

    FEAT: 지역 모델링 및 지역 별 관광지 뷰 구현

---
+++ SODAM/Sources/Region.swift
@@ -0,0 +1,16 @@
+//
+//  Region.swift
+//  SODAM
+//
+//  Created by 김용해 on 5/15/25.
+//
+
+import Foundation
+
+struct Region: Identifiable {
+    let id: UUID = UUID()
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    let position: CGPoint
+}
