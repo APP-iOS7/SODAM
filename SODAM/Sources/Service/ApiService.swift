133400b8abb186d5416e0fb0e6f78d3f724d57da
김용해 <yoho1019@gmail.com>
2025-05-19 10:08:16 +0900

    REFACTOR: MVVM 폴더 구조 변경

---
+++ SODAM/Sources/Service/ApiService.swift
@@ -0,0 +1,286 @@
+//
+//  APIService.swift
+//  SODAM
+//
+//  Created by 박세라 on 5/14/25.
+//
+import Foundation
+
+final class APIService {
+    
+    static let shared = APIService()
+    private init() {}
+    
+    /** 관광지 기본 정보 목록 조회
+     * - Parameters:
+     *      - numOfRows: 한 페이지 결과 수
+     *      - pageNo: 페이지 번호
+     * - Returns: 관광지 목록
+     */
+    func getThemeBasedList(numOfRows: Int, pageNo: Int) async throws -> [DetailModel] {
+        guard let urlString = buildThemeURL(path: APIConfig.apiUrl.themeBasedList, numOfRows: numOfRows, pageNo: pageNo) else {
+            throw NetworkManager.NetworkError.invalidURL
+        }
+        
+        let storyResponse: StoryResponse = try await NetworkManager.shared.fetchItemsAsync(from: urlString, headers: ["Content-Type":"application/json"])
+        let items = storyResponse.response.body.items.item
+        
+        return items
+    }
+    
+    /** 관광지 키워드 검색 목록 조회
+     * - Parameters:
+     *      - keyword: 검색할 키워드
+     *      - numOfRows: 한 페이지 결과 수
+     *      - pageNo: 페이지 번호
+     * - Returns: 관광지 목록
+     */
+    func getThemeSearchList(keyword: String, numOfRows: Int, pageNo: Int) async throws -> [DetailModel] {
+        // URL 빌드
+        guard let urlString = buildThemeURL(path: APIConfig.apiUrl.themeSearchList, keyword: keyword, numOfRows: numOfRows, pageNo: pageNo) else {
+            throw NetworkManager.NetworkError.invalidURL
+        }
+        
+        let storyResponse: StoryResponse = try await NetworkManager.shared.fetchItemsAsync(from: urlString, headers: ["Content-Type":"application/json"])
+        let items = storyResponse.response.body.items.item
+        return items
+    }
+    
+    
+    /** 관광지 위치기반 정보 목록 조회
+     * - Parameters:
+     *      - lng: 경도 - GPS X좌표(WGS84 경도 좌표) ex) 126.615455
+     *      - lat: 위도 - GPS Y좌표(WGS84 위도 좌표) ex) 34.476566
+     *      - radius: 거리 반경 : 거리 반경(단위:m) ,Max값 20000m=20Km
+     *      - numOfRows: 한 페이지 결과 수
+     *      - pageNo: 페이지 번호
+     * - Returns: 관광지 목록
+     */
+    func getThemeLocationBasedList(lng: Double, lat: Double, radius: Int, numOfRows: Int, pageNo: Int) async throws -> [DetailModel] {
+        guard let urlString = buildThemeURL(path: APIConfig.apiUrl.themeLocationBasedList, numOfRows: numOfRows, pageNo: pageNo, lng: lng, lat: lat, radius: radius) else {
+            throw NetworkManager.NetworkError.invalidURL
+        }
+        
+        let storyResponse: StoryResponse = try await NetworkManager.shared.fetchItemsAsync(from: urlString, headers: ["Content-Type":"application/json"])
+        let items = storyResponse.response.body.items.item
+        return items
+    }
+   
+    
+    /** 이야기 기본 정보 목록 조회
+     * - Parameters:
+     *      - numOfRows: 한 페이지 결과 수
+     *      - pageNo: 페이지 번호
+     *      - tid: 관광지아이디 ( key값으로 해당정보 조회시 tlid와 동시에 입력시 조회 가능함. )
+     *      - tlid: 관광지언어아이디( key값으로 해당정보 조회시 tid와 동시에 입력시 조회 가능함. )
+     * - Returns: 관광지 목록
+     */
+    func getStoryBasedList(numOfRows: Int, pageNo: Int, tid: Int? = nil, tlid: Int? = nil) async throws -> [DetailModel] {
+        guard let urlString = buildThemeURL(path: APIConfig.apiUrl.storyBasedList, numOfRows: numOfRows, pageNo: pageNo) else {
+            throw NetworkManager.NetworkError.invalidURL
+        }
+        
+        let storyResponse: StoryResponse = try await NetworkManager.shared.fetchItemsAsync(from: urlString, headers: ["Content-Type":"application/json"])
+        let items = storyResponse.response.body.items.item
+        return items
+    }
+    
+    /** 이야기 키워드 검색 목록 조회
+     * - Parameters:
+     *      - keyword: 검색할 키워드
+     *      - numOfRows: 한 페이지 결과 수
+     *      - pageNo: 페이지 번호
+     * - Returns: 관광지 목록
+     */
+    func getStorySearchList(keyword: String, numOfRows: Int, pageNo: Int) async throws -> [DetailModel] {
+        guard let urlString = buildThemeURL(path: APIConfig.apiUrl.storySearchList, keyword: keyword, numOfRows: numOfRows, pageNo: pageNo) else {
+            throw NetworkManager.NetworkError.invalidURL
+        }
+        
+        let storyResponse: StoryResponse = try await NetworkManager.shared.fetchItemsAsync(from: urlString, headers: ["Content-Type":"application/json"])
+        let items = storyResponse.response.body.items.item
+        
+        return items
+    }
+    
+    /** 이야기 위치기반 정보 목록 조회
+     * - Parameters:
+     *      - lng: 경도 - GPS X좌표(WGS84 경도 좌표) ex) 126.615455
+     *      - lat: 위도 - GPS Y좌표(WGS84 위도 좌표) ex) 34.476566
+     *      - radius: 거리 반경 : 거리 반경(단위:m) ,Max값 20000m=20Km
+     *      - numOfRows: 한 페이지 결과 수
+     *      - pageNo: 페이지 번호
+     * - Returns: 관광지 목록
+     */
+    func getStoryLocationBasedList(lng: Double, lat: Double, radius: Int, numOfRows: Int, pageNo: Int) async throws -> [DetailModel] {
+        guard let urlString = buildThemeURL(path: APIConfig.apiUrl.storyLocationBasedList, numOfRows: numOfRows, pageNo: pageNo, lng: lng, lat: lat, radius: radius) else {
+            throw NetworkManager.NetworkError.invalidURL
+        }
+        
+        let storyResponse: StoryResponse = try await NetworkManager.shared.fetchItemsAsync(from: urlString, headers: ["Content-Type":"application/json"])
+        let items = storyResponse.response.body.items.item
+        return items
+    }
+    
+    
+    /** 관광사진갤러리 목록 조회
+     * - Parameters:
+     *      - numOfRows: 한 페이지 결과 수
+     *      - pageNo: 페이지 번호
+     * - Returns: 관광지 목록
+     * - Description: 사진갤러리 목록을 조회하는 기능입니다. 제목으로 중복 콘텐츠를 제거하여 그룹화하고, 사진의 URL경로, 촬영월, 촬영장소 등의 내용을 목록으로 제공합니다.
+     */
+    func getGalleryList(numberOfRows: Int = 1, pageNo: Int = 1) async throws -> [GalleryItem] {
+        guard let urlString = buildGalleryURL(path: APIConfig.apiUrl.galleryList1, numOfRows: 10, pageNo: 1) else {
+            throw NetworkManager.NetworkError.invalidURL
+        }
+        
+        let galleryResponse: GalleryResponse = try await NetworkManager.shared.fetchItemsAsync(from: urlString, headers: ["Content-Type":"application/json"])
+        let items = galleryResponse.response.body.items.item
+        return items
+    }
+    
+    
+    /** 관광사진갤러리 상세 목록 조회
+     * - Parameters:
+     *      - numOfRows: 한 페이지 결과 수
+     *      - pageNo: 페이지 번호
+     *      - title: 요청 키워드(한글 경우, URL 인코딩 필요)
+     * - Returns: 관광지 목록
+     * - Description: 사진갤러리 상세 목록을 조회하는 기능입니다. 사진갤러리 목록 조회를 통해 제목에 해당하는 그룹화된 목록이며, 사진의 URL경로, 촬영월, 촬영장소 등의 내용을 목록으로 제공합니다
+     */
+    func getGalleryDetailList(numberOfRows: Int = 1, pageNo: Int = 1, title: String) async throws -> [GalleryItem] {
+        guard let urlString = buildGalleryURL(path: APIConfig.apiUrl.galleryDetailList1, numOfRows: 10, pageNo: 1, title: title) else {
+            throw NetworkManager.NetworkError.invalidURL
+        }
+        
+        let galleryResponse: GalleryResponse = try await NetworkManager.shared.fetchItemsAsync(from: urlString, headers: ["Content-Type":"application/json"])
+        let items = galleryResponse.response.body.items.item
+        return items
+    }
+    
+    
+    /** 관광사진갤러리 키워드 검색 목록 조회
+     * - Parameters:
+     *      - numOfRows: 한 페이지 결과 수
+     *      - pageNo: 페이지 번호
+     *      - keyword: 요청 키워드(한글 경우, URL 인코딩 필요)
+     * - Returns: 관광지 목록
+     * - Description: 키워드검색을 통해 사진갤러리 목록을 조회하는 기능입니다. 키워드검색을 통해 키워드 항목데이터와 매칭되는 정보를 목록으로 표출하며, 제목에 해당하는 그룹화된 목록을 제공합니다.
+     */
+    func getGallerySearchList(numberOfRows: Int = 1, pageNo: Int = 1, keyword: String) async throws -> [GalleryItem] {
+        guard let urlString = buildGalleryURL(path: APIConfig.apiUrl.gallerySearchList1, keyword: keyword, numOfRows: 10, pageNo: 1) else {
+            throw NetworkManager.NetworkError.invalidURL
+        }
+        
+        let galleryResponse: GalleryResponse = try await NetworkManager.shared.fetchItemsAsync(from: urlString, headers: ["Content-Type":"application/json"])
+        let items = galleryResponse.response.body.items.item
+        return items
+    }
+    
+    
+    func getAddress(x: Double, y: Double) async throws -> AddressResponse? {
+        guard let geocoderApiKey = Bundle.main.object(forInfoDictionaryKey: "GEOCODER_API_KEY") as? String else {
+            print("GeoCoder Api Key is missing in Info.plist")
+            return nil
+        }
+        
+        let urlString = "\(APIConfig.geocoderBaseURL)?service=address&request=getAddress&version=2.0&crs=epsg:4326&point=\(x), \(y)&format=json&type=both&zipcode=true&simple=false&key=\(geocoderApiKey)"
+        let addressResponse: AddressResponse = try await NetworkManager.shared.fetchItemsAsync(from: urlString, headers: ["Content-Type":"application/json"])
+        return addressResponse
+    }
+    
+    
+    
+    /// url query 구성 함수
+    func buildThemeURL(path: APIConfig.apiUrl, keyword: String? = nil, numOfRows: Int = 10, pageNo: Int = 1, lng: Double? = nil, lat: Double? = nil, radius: Int? = nil, tid: Int? = nil, tlid: Int? = nil, langCode: String = "ko") -> String? {
+        guard let tourApiKey = Bundle.main.object(forInfoDictionaryKey: "TOUR_API_KEY") as? String else {
+            print("Tour Api Key is missing in Info.plist")
+            return nil
+        }
+        
+        // serviceKey는 인코딩 하면 안됨
+        let baseWithKey = "\(APIConfig.audioBaseURL)/\(path)?serviceKey=\(tourApiKey)"
+        
+        var components = URLComponents()
+        
+        // 필수 queryItem
+        var items: [URLQueryItem] = [
+            URLQueryItem(name: "numOfRows", value: "\(numOfRows)"),
+            URLQueryItem(name: "pageNo", value: "\(pageNo)"),
+            URLQueryItem(name: "MobileOS", value: "IOS"),
+            URLQueryItem(name: "MobileApp", value: "AppTest"),
+            URLQueryItem(name: "langCode", value: langCode),
+            URLQueryItem(name: "_type", value: "json")
+        ]
+        
+        // 키워드가 있을 때만 query에 추가
+        if let keyword = keyword {
+            items.append(URLQueryItem(name:"keyword", value: keyword))
+        }
+        
+        // 위치기반일때만 추가
+        if let lng = lng, let lat = lat, let radius = radius {
+            items.append(URLQueryItem(name:"mapX", value: "\(lng)"))
+            items.append(URLQueryItem(name:"mapY", value: "\(lat)"))
+            items.append(URLQueryItem(name:"radius", value: "\(radius)"))
+        }
+        
+        // 이야기 기본 정보 목록에서 사용.
+        if let tid = tid, let tlid = tlid {
+            items.append(URLQueryItem(name:"tid", value: "\(tid)"))
+            items.append(URLQueryItem(name:"tlid", value: "\(tlid)"))
+        }
+        
+        components.queryItems = items
+        
+        
+        if let query = components.percentEncodedQuery {
+            return baseWithKey + "&" + query
+        } else {
+            return nil
+        }
+    }
+    
+    
+    /// url query 구성 함수
+    func buildGalleryURL(path: APIConfig.apiUrl, keyword: String? = nil, numOfRows: Int = 10, pageNo: Int = 1, title: String? = nil) -> String? {
+        guard let tourApiKey = Bundle.main.object(forInfoDictionaryKey: "TOUR_API_KEY") as? String else {
+            print("Tour Api Key is missing in Info.plist")
+            return nil
+        }
+        
+        // serviceKey는 인코딩 하면 안됨
+        let baseWithKey = "\(APIConfig.galleryBaseURL)/\(path)?serviceKey=\(tourApiKey)"
+        
+        var components = URLComponents()
+        
+        // 필수 queryItem
+        var items: [URLQueryItem] = [
+            URLQueryItem(name: "numOfRows", value: "\(numOfRows)"),
+            URLQueryItem(name: "pageNo", value: "\(pageNo)"),
+            URLQueryItem(name: "MobileOS", value: "IOS"),
+            URLQueryItem(name: "MobileApp", value: "AppTest"),
+            URLQueryItem(name: "_type", value: "json")
+        ]
+        
+        // 키워드가 있을 때만 query에 추가
+        if let keyword = keyword {
+            items.append(URLQueryItem(name:"keyword", value: keyword))
+        }
+        
+        // 타이틀이 있을 때만 query에 추가
+        if let title = title {
+            items.append(URLQueryItem(name:"title", value: title))
+        }
+        
+        components.queryItems = items
+        
+        
+        if let query = components.percentEncodedQuery {
+            return baseWithKey + "&" + query
+        } else {
+            return nil
+        }
+    }
+}
---
+++ SODAM/Sources/View/RegionalListView.swift
@@ -0,0 +1,90 @@
+//
+//  RegionalListView.swift
+//  SODAM
+//
+//  Created by 김용해 on 5/15/25.
+//
+
+import SwiftUI
+import UICommonExtension
+
+struct RegionalListView: View {
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    @Environment(\.dismiss) private var dismiss
+    var body: some View {
+        GeometryReader { geo in
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(regions) { region in
+                    NavigationLink(
+                        destination: RegionDetailListView(region: region)
+                    ) {
+                        gridItem(region: region, geo: geo)
+                    }
+                }
+            }
+            .padding()
+        }
+        .navigationTitle("지역 선택")
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                HStack {
+                    Image(systemName: "chevron.left")
+                    Text("뒤로가기")
+                }
+                .onTapGesture {
+                    dismiss()
+                }
+            }
+        }
+    }
+    
+    // MARK: 각 GridItem
+    private func gridItem(region: Region, geo: GeometryProxy) -> some View {
+        Image(region.imageName)
+            .resizable()
+            .frame(maxWidth: .infinity)
+            .frame(height: geo.size.height * 0.17)
+            .clipShape(.rect(cornerRadius: 15))
+            .overlay {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(.black.opacity(0.4))
+                    
+                    Text(region.name)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                }
+            }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        RegionalListView()
+    }
+}
+
+// MARK: 확장을 통해 데이터 가져오기
+extension RegionalListView {
+    var regions: [Region] {
+        [
+            Region(name: "서울", latitude: 37.5665, longitude: 126.9780, imageName: "Seoul"),
+            Region(name: "강원", latitude: 37.8228, longitude: 128.1555, imageName: "gangwon"),
+            Region(name: "경북", latitude: 36.4919, longitude: 128.8889, imageName: "gyeongbuk"),
+            Region(name: "경남", latitude: 35.4606, longitude: 128.2132, imageName: "gyeongnam"),
+            Region(name: "충북", latitude: 36.6357, longitude: 127.4917, imageName: "chungbuk"),
+            Region(name: "충남", latitude: 36.5184, longitude: 126.8000, imageName: "chungnam"),
+            Region(name: "경기", latitude: 37.4138, longitude: 127.5183, imageName: "gyeonggi"),
+            Region(name: "제주", latitude: 33.3717, longitude: 126.5520, imageName: "jeju"),
+            Region(name: "전북", latitude: 35.7167, longitude: 127.1441, imageName: "jeonbuk"),
+            Region(name: "전남", latitude: 34.8746, longitude: 126.9865, imageName: "jeonnam")
+        ]
+    }
+
+}
