75d8aa6570fa175a8d9bce4f3f61244bcc046923
hajinCHOI <cresent0203@gmail.com>
2025-05-22 11:46:55 +0900

    REFACTOR: 오디오플레이어 싱글톤으로 변경

--- SODAM/Sources/ViewModel/PlayerViewModel.swift
+++ SODAM/Sources/ViewModel/PlayerViewModel.swift
@@ -3,35 +3,33 @@
 //  SODAM
 //
 //  Created by 최하진 on 5/14/25.
-//
-
 import Foundation
 import AVFoundation
 
 class PlayerViewModel: ObservableObject {
+    static let shared = PlayerViewModel()
+    
     @Published var isPlaying: Bool = false
     @Published var isLongVer: Bool = true
-    @Published var title: String
-    @Published var imageUrl: String
-    @Published var audioUrl: String
     @Published var audioData: Data?
-    var audioPlayer: AVPlayer?
-    init(title: String, imageUrl: String, audioUrl: String) {
-        self.title = title
-        self.audioUrl = audioUrl
-        self.imageUrl = imageUrl
-        setPlayer()
-        play()
+    @Published var playModel: DetailModel? {
+        didSet {
+            if playModel != nil {
+                setPlayer()
+                play()
+            }
+                }
     }
+    
+    var audioPlayer: AVPlayer?
 
-    /**플레이어 세팅
-     */
+    /**플레이어 세팅*/
     private func setPlayer() {
         do {
             // 무음모드에서도 재생되도록 설정
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [])
             try AVAudioSession.sharedInstance().setActive(true) // 오디오 세션 활성화. 앱이 백그라운드로 이동하거나 중단되었을 때, 다시 활성화해야함
-            guard let url = URL.init(string: audioUrl) else { return }
+            guard let url = URL.init(string: playModel?.audioUrl ?? "") else { return }
             let playerItem = AVPlayerItem(url: url)
             audioPlayer = AVPlayer.init(playerItem: playerItem)
         } catch {
@@ -39,28 +37,27 @@ class PlayerViewModel: ObservableObject {
         }
     }
     
-    /**오디오 재생
-     */
+    /**오디오 재생*/
     func play() {
         audioPlayer?.play()
         self.isPlaying = true
        }
     
-    /**오디오 정지
-     */
+    /**오디오 정지*/
     func pause() {
         audioPlayer?.pause()
         self.isPlaying = false
     }
     
     /**이미지URL호출
+     * - Returns: 이미지 URL
      */
     func getImageURL() -> URL? {
         //모델 변경 시 if let이나 guard let 사용
-            return URL(string: imageUrl)
+        return URL(string: playModel?.imageUrl ?? "")
     }
     
     func getTitle() -> String {
-        return title
+        return playModel?.title ?? ""
     }
 }
