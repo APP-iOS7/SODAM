1f01e939150c58ac79b7cd37f94a509f2ba82e23
hajinCHOI <cresent0203@gmail.com>
2025-05-27 11:38:52 +0900

    FEAT: 교육콘텐츠 목록별 호출

--- SODAM/Sources/View/EducationListView.swift
+++ SODAM/Sources/View/EducationListView.swift
@@ -2,12 +2,13 @@ import SwiftUI
 
 struct EducationListView: View {
     
-    var category: String
-    @StateObject var educationListViewModel = EducationListViewModel()
+//    var category: String
+    @StateObject var educationListViewModel: EducationListViewModel
     
     var body: some View {
         NavigationStack {
             VStack {
+                Divider()
                 ForEach(educationListViewModel.spots, id: \.self) { spot in
                     NavigationLink {
                         //TODO: DetailView 연결
@@ -15,6 +16,7 @@ struct EducationListView: View {
                     } label: {
                         EducationListCellView(spot: spot)
                     }
+                    Divider()
                 }
                 Spacer()
             }
@@ -38,7 +40,8 @@ struct EducationListCellView: View {
                 VStack(alignment: .leading) {
                     Text(spot.title)
                         .foregroundStyle(Color.textColor)
-                    Text(spot.addr1 ?? "" )
+                        .padding(.top, 2)
+                    Text("\(spot.addr1 ?? "") \(spot.addr2 ?? "")")
                         .font(.caption)
                         .foregroundStyle(.gray)
                 }
@@ -46,13 +49,13 @@ struct EducationListCellView: View {
                 Spacer()
             }
             HStack(alignment: .center) {
-                Text("\(String(format: "%02d", (Int(spot.playTime ?? "0") ?? 0) / 60)):\(String(format: "%02d", Int(spot.playTime ?? "0") ?? 0 % 60))")
+                Text("\(String(format: "%02d", (Int(spot.playTime ?? "0") ?? 0) / 60)):\(String(format: "%02d", (Int(spot.playTime ?? "0") ?? 0 ) % 60))")
                 Image(systemName: "play.circle")
             }
             .foregroundStyle(Color.textColor)
-            .padding(.trailing, 30)
+            .padding(.trailing, 20)
         }
-        .padding([.leading, .trailing], 5)
+        .padding([.leading, .trailing], 15)
     }
 }
 
