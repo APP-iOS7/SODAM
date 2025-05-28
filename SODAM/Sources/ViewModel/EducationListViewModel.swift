4e5a2e664e10753b352875ec81d840fd8ed40024
hajinCHOI <cresent0203@gmail.com>
2025-05-28 10:30:37 +0900

    FEAT: 주소 호출

--- SODAM/Sources/ViewModel/EducationListViewModel.swift
+++ SODAM/Sources/ViewModel/EducationListViewModel.swift
@@ -1,12 +1,36 @@
 import Foundation
 
 class EducationListViewModel: ObservableObject {
-//    @Published var category: String
+    //    @Published var category: String
     @Published var spots: [DetailModel]
-    //TODO: 초등교육 콘텐츠 카테고리별로 불러오기
     
     init(spots: [DetailModel]) {
         self.spots = spots
+        getAddress()
     }
-
+    
+    /**관광지 주소 받아오기*/
+    func getAddress() {
+        Task {
+            do{
+                for i in 0..<spots.count {
+                    print("1")
+                    if let x = Double(spots[i].mapX), let y = Double(spots[i].mapY) {
+//                        let address = try await APIService.shared.getAddress(x: x, y: y)?.response.result?.last?.text?.split(separator: " ")
+                        let addr1 = try await APIService.shared.getAddress(x: x, y: y)?.response.result?.last?.structure?.level1
+                        let addr2 = try await APIService.shared.getAddress(x: x, y: y)?.response.result?.last?.structure?.level2
+                        await self.updateProperties(addr1: String(addr1 ?? ""), addr2: String(addr2 ?? ""), i: i)
+                    }
+                }
+                
+            } catch {
+                print("get Address error: \(error)")
+            }
+        }
+    }
+    
+    @MainActor func updateProperties(addr1: String, addr2: String, i: Int) {
+        spots[i].addr1 = addr1
+        spots[i].addr2 = addr2
+    }
 }
