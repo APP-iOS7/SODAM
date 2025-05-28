054f0d90cdb24885c85da59641aee9d8f2eb2be1
hajinCHOI <cresent0203@gmail.com>
2025-05-28 10:45:35 +0900

    CHORE: 메뉴 높이 설정 방식 변경

--- SODAM/Sources/View/EducationView.swift
+++ SODAM/Sources/View/EducationView.swift
@@ -3,31 +3,33 @@ import SwiftUI
 struct EducationView: View {
     @StateObject var edcationViewModel = EducationViewModel()
     var body: some View {
-        NavigationStack {
-            VStack {
-                ForEach(edcationViewModel.educations) { education in
-                    NavigationLink {
-                        EducationListView(educationListViewModel: EducationListViewModel(spots: education.lists))
-                    } label: {
-                        RoundedRectangle(cornerRadius: 10)
-                            .frame(height: 150)
-                            .padding([.leading, .trailing], 5)
-                            .foregroundStyle(education.color)
-                            .overlay(
-                                ZStack {
-                                    HStack {
-                                        Spacer()
-                                        VStack {
+        GeometryReader { geo in
+            NavigationStack {
+                VStack {
+                    ForEach(edcationViewModel.educations) { education in
+                        NavigationLink {
+                            EducationListView(educationListViewModel: EducationListViewModel(spots: education.lists))
+                        } label: {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(height: geo.size.height / 4.3)
+                                .padding([.leading, .trailing], 5)
+                                .foregroundStyle(education.color)
+                                .overlay(
+                                    ZStack {
+                                        HStack {
                                             Spacer()
-                                            Text("\(education.name)")
-                                                .font(.title)
-                                                .foregroundStyle(Color.textColor)
+                                            VStack {
+                                                Spacer()
+                                                Text("\(education.name)")
+                                                    .font(.title)
+                                                    .foregroundStyle(Color.textColor)
+                                            }
+                                            .padding(10)
                                         }
                                         .padding(10)
                                     }
-                                    .padding(10)
-                                }
-                            )
+                                )
+                        }
                     }
                 }
             }
