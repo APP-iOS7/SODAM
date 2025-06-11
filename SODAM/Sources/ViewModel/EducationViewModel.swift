d3497e33578f2b0a17ea92a0ef7a632f45aa3696
hajinCHOI <cresent0203@gmail.com>
2025-06-11 13:07:40 +0900

    ADD: EducationViewModel 주석정리 및 불필요 코드 제거

--- SODAM/Sources/ViewModel/EducationViewModel.swift
+++ SODAM/Sources/ViewModel/EducationViewModel.swift
@@ -1,31 +1,30 @@
 import Foundation
 import SwiftUICore
 
+/// 초등교육콘텐츠 모델
 struct Education: Identifiable {
     let id = UUID()
     var name: String
     var color: Color
-    var imageUrl: String
     var category: String
     var lists: [DetailModel]
 }
 
 class EducationViewModel: ObservableObject {
-    @Published var lists: [[DetailModel]] = []
     @Published var educations: [Education] = [
-        Education(name: "교과서 속 문화 여행", color: Color.secondaryColorBlue, imageUrl: "https://sfj608538-sfj608538.ktcdn.co.kr/file/image/service/11153.jpg", category: "C", lists: []),
-        Education(name: "교과서 속 역사 여행", color: Color.secondaryColorPurple, imageUrl: "https://sfj608538-sfj608538.ktcdn.co.kr/file/image/service/11153.jpg", category: "H", lists: []),
-        Education(name: "교과서 속 인물 여행", color: Color.secondaryColorRed, imageUrl: "https://sfj608538-sfj608538.ktcdn.co.kr/file/image/service/11153.jpg", category: "P", lists: []),
-        Education(name: "교과서 속 과학 여행", color: Color.secondaryColorYellow, imageUrl: "https://sfj608538-sfj608538.ktcdn.co.kr/file/image/service/11153.jpg", category: "S", lists: []),
-        
+        Education(name: "교과서 속 문화 여행", color: Color.secondaryColorBlue, category: "C", lists: []),
+        Education(name: "교과서 속 역사 여행", color: Color.secondaryColorPurple, category: "H", lists: []),
+        Education(name: "교과서 속 인물 여행", color: Color.secondaryColorRed, category: "P", lists: []),
+        Education(name: "교과서 속 과학 여행", color: Color.secondaryColorYellow, category: "S", lists: []),
     ]
     
-    //TODO: 초등교육 콘텐츠 카테고리별로 불러오기
     init() {
         fetchLists()
     }
+    
+    /// 초등콘텐츠 카테고리별 데이터 패치
     private func fetchLists() {
-        lists = APIService.shared.loadJSONData() ?? []
+        let lists = APIService.shared.loadJSONData() ?? []
         for i in 0...3 {
             educations[i].lists = lists[i]
         }
